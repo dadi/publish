7fa2f738478d3a4efc165e748dcad7268174bf12
mingard <info@arthurmingard.com>
2017-02-10 15:34:55 +0000

    refactor: Remove unnecessary components

--- dadi/frontend/views/StyleGuide/StyleGuide.jsx
+++ dadi/frontend/views/StyleGuide/StyleGuide.jsx
@@ -3,11 +3,7 @@ import { connect } from 'preact-redux'
 import { bindActionCreators } from 'redux'
 import { connectHelper } from '../../lib/util'
 
-import * as userActions from '../../actions/userActions'
-
 import Main from '../../components/Main/Main'
-import Card from '../../components/Card/Card'
-import CardGroup from '../../components/CardGroup/CardGroup'
 import Select from '../../components/Select/Select'
 import SubmitButton from '../../components/SubmitButton/SubmitButton'
 import TextInput from '../../components/TextInput/TextInput'
@@ -29,55 +25,33 @@ class StyleGuide extends Component {
   render() {
     return (
       <div>
-        <CardGroup>
-          <Card>
-            <h2>TextInput</h2>
-            <TextInput />
-          </Card>
-          <Card>
-            <h2>SubmitButton</h2>
-            <SubmitButton />
-          </Card>
-        </CardGroup>
-
-        <CardGroup>
-          <Card>
-            <h2>Select (searchable, with options)</h2>
-            <Select
-                searchable={true}
-                options={this.selectOptions} />
-          </Card>
-          <Card>
-            <h2>Select (searchable, with options, limit 3)</h2>
-            <Select
-                limit={3}
-                searchable={true}
-                options={this.selectOptions} />
-          </Card>
-        </CardGroup>
-
-        <CardGroup>
-          <Card>
-            <h2>Select (not searchable, with options)</h2>
-            <Select
-                searchable={false}
-                options={this.selectOptions} />
-          </Card>
-          <Card>
-            <h2>Select (not searchable, with options, limit 3)</h2>
-            <Select
-                limit={3}
-                searchable={false}
-                options={this.selectOptions} />
-          </Card>
-        </CardGroup>
-
+        <h2>TextInput</h2>
+        <TextInput />
+        <h2>SubmitButton</h2>
+        <SubmitButton />
+        <h2>Select (searchable, with options)</h2>
+        <Select
+          searchable={true}
+          options={this.selectOptions} />
+        <h2>Select (searchable, with options, limit 3)</h2>
+        <Select
+          limit={3}
+          searchable={true}
+          options={this.selectOptions} />
+        <h2>Select (not searchable, with options)</h2>
+        <Select
+          searchable={false}
+          options={this.selectOptions} />
+        <h2>Select (not searchable, with options, limit 3)</h2>
+        <Select
+          limit={3}
+          searchable={false}
+          options={this.selectOptions} />
       </div>
     )
   }
 }
 
 export default connectHelper(
-  state => state,
-  dispatch => bindActionCreators(userActions, dispatch)
+  dispatch => bindActionCreators({}, dispatch)
 )(StyleGuide)
