a11de2681d7e66e41773c4151e2d0f5dccb85c23
mingard <info@arthurmingard.com>
2017-02-22 15:57:22 +0000

    chore: remove line spaces

--- dadi/lib/watch/index.js
+++ dadi/lib/watch/index.js
@@ -31,9 +31,7 @@ Watch.prototype.start = function () {
   // Start watching directories for compile
   return this
 }
-
 module.exports = function () {
   return new Watch()
 }
-
 module.exports.Watch = Watch
