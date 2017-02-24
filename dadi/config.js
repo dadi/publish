914c29af279fceb65c2705844b6a06376ade57e7
mingard <info@arthurmingard.com>
2017-02-24 13:10:48 +0000

    feat: global schema appended with field schema

--- dadi/config.js
+++ dadi/config.js
@@ -2,8 +2,37 @@
 
 const convict = require('convict')
 const path = require('path')
+const fs = require('fs')
 // Define a schema
-const conf = convict(require('./config-schema'))
+const schema = require('./config-schema')
+
+/**
+ * Get Adittional Schema
+ * @param  {String} dir   Directory to searcg
+ * @param  {Regex} match Filename regular expression
+ * @return {undefined}
+ */
+const getAdditionalSchema = (dir, match) => {
+  fs.readdirSync(dir).forEach((folder) => {
+    let sub = path.resolve(dir,folder)
+
+    if (fs.lstatSync(sub).isDirectory()) {
+      getAdditionalSchema(sub, match)
+
+    } else if (fs.lstatSync(sub).isFile()) {
+      let file = path.parse(sub)
+
+      if (file.ext === '.js' && file.name.match(match)) {
+        Object.assign(schema, require(sub))
+      }
+    }
+  })
+}
+
+// Fetch aditional component schema
+getAdditionalSchema(path.resolve(__dirname, './frontend/components'), /ConfigSchema$/g)
+
+const conf = convict(schema)
 
 // Load environment dependent configuration
 const env = conf.get('env')
