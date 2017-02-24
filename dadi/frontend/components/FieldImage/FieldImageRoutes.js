7d4e9703ae20343163f3ddec60b01dd306337c7c
mingard <info@arthurmingard.com>
2017-02-24 20:24:25 +0000

    feat: signed url method

--- dadi/frontend/components/FieldImage/FieldImageRoutes.js
+++ dadi/frontend/components/FieldImage/FieldImageRoutes.js
@@ -7,29 +7,48 @@ const ImageFieldRoute = function (app) {
 
   if (config.get('Image.s3.enabled')) {
     this.initAWS()
-    this.s3 = this.initS3()
+    this.S3 = this.initS3()
+
     app.get({
       name: 'images3sign', // This allows us to reuse the auth request
       path: '/fields/image/s3/sign'
     }, 
     (req, res, next) => {
       res.header('Content-Type', 'application/json')
+      let response = {route: req.route.name}
+
       if (req.isAuthenticated()) {
-        return this.refreshTemporaryCredentials().then(token => {
-          res.write(JSON.stringify({route: req.route.name, token: AWS.config.credentials.sessionToken}))
+        if (req.query.fileName) {
+          return this.getSignedResponse(req.query.fileName).then(resp => {
+            res.write(JSON.stringify(Object.assign(response, resp)))
+            res.end()
+            return next()
+          })
+        } else {
+          res.write(JSON.stringify( Object.assign(response, {err: 'Missing fileName'}) ))
           res.end()
           return next()
-        })
+        }
       } else {
-        res.write(JSON.stringify({route: req.route.name, authorised: false}))
+        res.write(JSON.stringify( Object.assign(response, {authorised: false}) ))
         res.end()
         return next()
       }
       
     })
   }
-
 }
+
+ImageFieldRoute.prototype.getSignedResponse = function (fileName) {
+  return this.getS3SignedUrl(fileName).then(resp => {
+    if (resp.err) {
+      return {err: resp.err}
+    } else {
+      return {url: resp.url}
+    }
+  })
+}
+
 // S3
 ImageFieldRoute.prototype.initAWS = function () {
   AWS.config.update({
@@ -37,20 +56,21 @@ ImageFieldRoute.prototype.initAWS = function () {
     secretAccessKey: config.get('Image.s3.secretAccessKey')
   })
 
-  if (config.get('Image.s3.region') && config.get('Image.s3.region') != "") {
+  if (config.get('Image.s3.region') && config.get('Image.s3.region') != '') {
     AWS.config.update({region: config.get('Image.s3.region')})
   }
-  AWS.config.credentials = new AWS.TemporaryCredentials()
 }
 
 ImageFieldRoute.prototype.initS3 = function () {
   return new AWS.S3({httpOptions: { timeout: 480000 }})
 }
 
-
-ImageFieldRoute.prototype.refreshTemporaryCredentials = function () {
+ImageFieldRoute.prototype.getS3SignedUrl = function (fileName) {
   return new Promise((resolve, reject) => {
-    return AWS.config.credentials.refresh(resolve)
+    return this.S3
+    .getSignedUrl('putObject', {Bucket: config.get('Image.s3.bucketName'), Key: fileName}, (err, url) => {
+      return resolve({err, url})
+    })
   })
 }
 
