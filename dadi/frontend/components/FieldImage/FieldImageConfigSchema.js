cffeed8fbf5d4bc4e473cb28a9be1a97fdd664ba
mingard <info@arthurmingard.com>
2017-02-27 10:05:49 +0000

    feat: add env to field schema

--- dadi/frontend/components/FieldImage/FieldImageConfigSchema.js
+++ dadi/frontend/components/FieldImage/FieldImageConfigSchema.js
@@ -23,22 +23,26 @@ module.exports = {
       accessKeyId: {
         doc: "AWS Access Key ID",
         format: String,
-        default: ""
+        default: "",
+        env: "AWS_ACCESS_KEY"
       },
       secretAccessKey: {
         doc: "AWS Secret Key",
         format: String,
-        default: ""
+        default: "",
+        env: "AWS_SECRET_KEY"
       },
       bucketName: {
         doc: "AWS Bucket Name",
         format: String,
-        default: ""
+        default: "",
+        env: "AWS_S3_BUCKET"
       },
       region: {
         doc: "AWS Region",
         format: String,
-        default: ""
+        default: "",
+        env: "AWS_REGION"
       }
     }
   }
