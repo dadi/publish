e48b4bb745b31f97b5917c2fa4f8aec71ef49b73
Eduardo Bouças <mail@eduardoboucas.com>
2017-03-04 11:29:31 +0000

    Add selection to document list view (#51)
    * fix: various fixes to mobile menu
    
    * feat: add selection to document list view
    
    * feat: add selection to document list view

--- dadi/frontend/components/Table/Table.jsx
+++ dadi/frontend/components/Table/Table.jsx
@@ -16,16 +16,89 @@ export default class Table extends Component {
     selectable: true
   }
 
+  constructor(props) {
+    super(props)
+
+    this.state.selectedRows = {}
+  }
+
+  handleHeadSelect(event) {
+    const selected = event.target.checked
+
+    let newSelectedRows = {}
+
+    if (selected) {
+      for (let i = 1; i < this.props.children.length; i++) {
+        newSelectedRows[i] = true
+      }
+    }
+
+    this.setState({
+      selectedRows: newSelectedRows
+    })
+  }
+
+  handleRowSelect(index, event) {
+    const {selectedRows} = this.state
+    const selected = event.target.checked
+    const isRangeSelection = event.shiftKey
+    const selectedRowsIndices = Object.keys(selectedRows)
+
+    let newSelectedRows = Object.assign({}, selectedRows, {
+      [index]: selected
+    })
+
+    if (isRangeSelection && selectedRowsIndices.length) {
+      let rangeBoundary = Infinity
+
+      selectedRowsIndices.sort().forEach(selectedRowIndex => {
+        if (Math.abs(rangeBoundary - index) > Math.abs(selectedRowIndex - index)) {
+          rangeBoundary = selectedRowIndex
+        }
+      })
+
+      const selectDown = rangeBoundary >= index
+      const rangeFrom = selectDown ? index : rangeBoundary
+      const rangeTo = selectDown ? rangeBoundary : index
+
+      for (let i = rangeFrom; i <= rangeTo; i++) {
+        newSelectedRows[i] = selected
+      }
+    }
+
+    this.setState({
+      selectedRows: newSelectedRows
+    })
+  }
+
   groupChildren() {
+    const {fillBlanks, selectable} = this.props
+    const {selectedRows} = this.state
+    const selectedRowsIndices = Object.keys(selectedRows).filter(index => selectedRows[index])
+
     let head = []
     let body = []
     
-    this.props.children.forEach(child => {
-      child.attributes = Object.assign({}, child.attributes, this.props)
+    this.props.children.forEach((child, index) => {
+      let childAttributes = Object.assign({}, child.attributes, {
+        fillBlanks,
+        selectable,
+        tableIndex: index
+      })
 
       if (child.nodeName.name === 'TableHead') {
+        childAttributes.onSelect = this.handleHeadSelect.bind(this)
+        childAttributes.allSelected = selectedRowsIndices.length === (this.props.children.length - 1)
+
+        child.attributes = childAttributes
+
         head.push(child)
       } else {
+        childAttributes.selected = selectedRows[index] === true
+        childAttributes.onSelect = this.handleRowSelect.bind(this)
+
+        child.attributes = childAttributes
+
         body.push(child)
       }
     })
@@ -55,6 +128,14 @@ export default class Table extends Component {
 // Table row
 //
 class TableRow extends Component {
+  handleSelectClick(event) {
+    const {onSelect, tableIndex} = this.props
+
+    if (typeof onSelect === 'function') {
+      onSelect(tableIndex, event)
+    }
+  }
+
   renderChildren() {
     return this.props.children.map(child => {
       child.attributes = child.attributes || {}
@@ -65,14 +146,24 @@ class TableRow extends Component {
   }
 
   render() {
-    let select = this.props.selectable ?
-      <TableRowCell><input class={styles.select} type="checkbox" /></TableRowCell>
-      : null
+    const {selected} = this.props
 
+    let rowStyle = new Style(styles, 'row')
+
+    rowStyle.addIf('row-selected', selected)
 
     return (
-      <tr class={styles.row}>
-        {select}
+      <tr class={rowStyle.getClasses()}>
+        {this.props.selectable &&
+          <TableRowCell select={true}>
+            <input
+              checked={selected}
+              class={styles.select}
+              type="checkbox"
+              onClick={this.handleSelectClick.bind(this)}
+            />
+          </TableRowCell>
+        }
         {this.renderChildren()}
       </tr>
     )
@@ -85,13 +176,16 @@ class TableRow extends Component {
 class TableRowCell extends Component {
   render() {
     let children = this.props.children
+    let cellStyle = new Style(styles, 'cell')
+
+    cellStyle.addIf('select-cell', this.props.select)
     
     if (!children.length && this.props.fillBlanks) {
       children = <span class={styles['row-cell-blank']}>None</span>
     }
 
     return (
-      <td class={styles['row-cell']}>
+      <td class={cellStyle.getClasses()}>
         {children}
       </td>
     )
@@ -102,14 +196,29 @@ class TableRowCell extends Component {
 // Table head
 //
 class TableHead extends Component {
+  handleSelectClick(event) {
+    const {onSelect} = this.props
+
+    if (typeof onSelect === 'function') {
+      onSelect(event)
+    }
+  }
+
   render() {
-    let select = this.props.selectable ?
-      <TableHeadCell><input class={styles.select} type="checkbox" /></TableHeadCell>
-      : null
+    const {allSelected} = this.props
 
     return (
       <thead class={styles.head}>
-        {select}
+        {this.props.selectable &&
+          <TableHeadCell select={true}>
+            <input
+              checked={allSelected}
+              class={styles.select}
+              type="checkbox"
+              onClick={this.handleSelectClick.bind(this)}
+            />
+          </TableHeadCell>
+        }
         {this.props.children}
       </thead>
     )
@@ -126,14 +235,19 @@ class TableHeadCell extends Component {
   }
 
   render() {
+    const {arrow} = this.props
+    let cellStyle = new Style(styles, 'cell', 'head-cell')
+
+    cellStyle.addIf('select-cell', this.props.select)
+
     return (
-      <th class={styles['head-cell']}>
-        {this.props.arrow && ['up', 'down'].includes(this.props.arrow) &&
+      <th class={cellStyle.getClasses()}>
+        {arrow && ['up', 'down'].includes(arrow) &&
           <IconArrow
             class={styles['head-cell-arrow']}
             width="10"
             height="10"
-            direction={this.props.arrow}
+            direction={arrow}
           />
         }
 
