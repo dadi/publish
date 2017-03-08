79ca8d9248f710f35a4cb38ce7506774402b23de
mingard <info@arthurmingard.com>
2017-03-08 13:54:40 +0000

    chore: remove thunk

--- dadi/frontend/index.jsx
+++ dadi/frontend/index.jsx
@@ -1,10 +1,9 @@
 'use strict'
-import { h, render } from 'preact'
-import { Provider } from 'preact-redux'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
+import {h, render} from 'preact'
+import {Provider} from 'preact-redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 import createHistory from 'history/createBrowserHistory'
-import { syncHistoryWithStore } from 'preact-router-redux'
+import {syncHistoryWithStore} from 'preact-router-redux'
 
 import * as reducers from 'reducers'
 import * as types from 'actions/actionTypes'
@@ -13,7 +12,6 @@ import App from 'containers/App/App'
 
 const browserHistory = createHistory()
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 const reducer = combineReducers(reducers)
 const store = createStore(reducer)
 
