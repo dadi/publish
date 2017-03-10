9f8456b33ac14c3a6ca80641cbb6b4d940c53abb
Eduardo Bouças <mail@eduardoboucas.com>
2017-03-10 18:38:45 +0000

    fix: add check for document.local and change title

--- dadi/frontend/lib/util.js
+++ dadi/frontend/lib/util.js
@@ -52,8 +52,8 @@ export function slugify(str) {
     .toLowerCase()
     .replace(/\s+/g, '-')     // Replace spaces with -
     .replace(/\/+/g, '-')     // Replace slashes with -
-    .replace(/[^\w-]+/g, '') // Remove all non-word chars
-    .replace(/--+/g, '-')   // Replace multiple - with single -
+    .replace(/[^\w-]+/g, '')  // Remove all non-word chars
+    .replace(/--+/g, '-')     // Replace multiple - with single -
     .replace(/^-+/, '')       // Trim - from start of text
     .replace(/-+$/, '')       // Trim - from end of text
 }
@@ -72,4 +72,8 @@ export function debounce(func, wait, immediate) {
     timeout = setTimeout(later, wait)
     if (callNow) func.apply(context, args)
   }
+}
+
+export function setPageTitle(title) {
+  document.title = `${title} / DADI Publish`
 }
