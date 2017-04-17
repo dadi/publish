2dc47e56917b73b9fc1692054e65d9b05ddaf89e
Eduardo Bouças <mail@eduardoboucas.com>
2017-04-17 18:18:27 +0100

    feat: add user actions

--- frontend/actions/userActions.js
+++ frontend/actions/userActions.js
@@ -1,4 +1,9 @@
+import * as Constants from 'lib/constants'
 import * as Types from 'actions/actionTypes'
+import * as documentActions from './documentActions'
+
+import {getApiForUrlParams, getCollectionForUrlParams} from 'lib/collection-lookup'
+import apiBridgeClient from 'lib/api-bridge-client'
 
 export function setRemoteUser (user) {
   return {
@@ -7,10 +12,57 @@ export function setRemoteUser (user) {
   }
 }
 
-export function updateLocalUser (data) {
+export function updateLocalUser () {
+  return (dispatch, getState) => {
+    const apis = getState().api.apis
+    const currentUser = getState().user.remote
+    const authApi = getApiForUrlParams(apis, {
+      collection: Constants.AUTH_COLLECTION
+    })
+    const authCollection = getCollectionForUrlParams(apis, {
+      collection: Constants.AUTH_COLLECTION,
+      useApi: authApi
+    })
+
+    apiBridgeClient(authApi)
+      .in(authCollection.name)
+      .whereFieldIsEqualTo('_id', currentUser._id)
+      .find()
+      .then(response => {
+        if (response.results && response.results.length) {
+          dispatch(setRemoteUser(response.results[0]))
+        }
+      })
+  }
+}
+
+export function saveUser ({api, collection, user}) {
+  return (dispatch, getState) => {
+    const currentUser = getState().user.remote
+
+    dispatch(documentActions.setRemoteDocumentStatus(Constants.STATUS_SAVING))
+
+    apiBridgeClient(api)
+      .in(collection.name)
+      .whereFieldIsEqualTo('_id', currentUser._id)
+      .update(user)
+      .then(response => {
+        if (response.results && response.results.length) {
+          const newUser = response.results[0]
+
+          dispatch(documentActions.setRemoteDocument(newUser, true, true))
+          dispatch(setRemoteUser(newUser))
+        } else {
+          dispatch(documentActions.setRemoteDocumentStatus(Constants.STATUS_FAILED))
+        }
+      })
+  }
+}
+
+export function setUserStatus (status) {
   return {
-    data,
-    type: Types.UPDATE_LOCAL_USER
+    status,
+    type: Types.SET_USER_STATUS
   }
 }
 
