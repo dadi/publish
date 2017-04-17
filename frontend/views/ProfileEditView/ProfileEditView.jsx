33e27a1ececbc1d80765299d8c5c273a51bd9c23
Eduardo Bouças <mail@eduardoboucas.com>
2017-04-17 18:27:27 +0100

    refactor: add onBuildSectionUrl to profile view

--- frontend/views/ProfileEditView/ProfileEditView.jsx
+++ frontend/views/ProfileEditView/ProfileEditView.jsx
@@ -6,6 +6,7 @@ import Header from 'containers/Header/Header'
 import Main from 'components/Main/Main'
 import Page from 'components/Page/Page'
 import ProfileEdit from 'containers/ProfileEdit/ProfileEdit'
+import ProfileEditToolbar from 'containers/ProfileEditToolbar/ProfileEditToolbar'
 
 import {setPageTitle} from 'lib/util'
 
@@ -23,14 +24,25 @@ export default class ProfileEditView extends Component {
 
         <Main>
           <ProfileEdit
-            collection={collection}
-            group={group}
-            onPageTitle={this.handlePageTitle}
+            onBuildSectionUrl={this.handleBuildSectionUrl.bind(this)}
             section={section}
           />
         </Main>
+
+        <ProfileEditToolbar />
       </Page>
     ) 
+  }
+
+  handleBuildSectionUrl() {
+    const {
+      collection,
+      documentId,
+      group,
+      section
+    } = this.props
+
+    return ['profile']
   }
 
   handlePageTitle(title) {
