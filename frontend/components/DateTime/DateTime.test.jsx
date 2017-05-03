ab81083d2e6e04953bd3574af47a08d9a93284fb
Eduardo Bouças <mail@eduardoboucas.com>
2017-05-03 11:31:21 +0100

    test: comment out temporary test

--- frontend/components/DateTime/DateTime.test.jsx
+++ frontend/components/DateTime/DateTime.test.jsx
@@ -1,77 +1,77 @@
-import {h, options, render} from 'preact'
-import {expect} from 'chai'
+// import {h, options, render} from 'preact'
+// import {expect} from 'chai'
 
-import Checkbox from './Checkbox'
+// import Checkbox from './Checkbox'
 
-// DOM setup
-let $, mount, root, scratch
+// // DOM setup
+// let $, mount, root, scratch
 
-options.debounceRendering = f => f()
+// options.debounceRendering = f => f()
 
-beforeAll(() => {
-  $ = (sel, all) => all ? scratch.querySelectorAll(sel) : scratch.querySelector(sel)
-  mount = jsx => root = render(jsx, scratch, root)
-  scratch = document.createElement('div')
-})
+// beforeAll(() => {
+//   $ = (sel, all) => all ? scratch.querySelectorAll(sel) : scratch.querySelector(sel)
+//   mount = jsx => root = render(jsx, scratch, root)
+//   scratch = document.createElement('div')
+// })
 
-afterEach(() => {
-  mount(null).remove()
-})
+// afterEach(() => {
+//   mount(null).remove()
+// })
 
-describe.only('Checkbox component', () => {
-  it('has propTypes', () => {
-    const button = (
-      <Checkbox />
-    )
+// describe.only('Checkbox component', () => {
+//   it('has propTypes', () => {
+//     const button = (
+//       <Checkbox />
+//     )
 
-    expect(button.nodeName.propTypes).to.exist
-    expect(Object.keys(button.nodeName.propTypes)).to.have.length.above(0)
-  })
+//     expect(button.nodeName.propTypes).to.exist
+//     expect(Object.keys(button.nodeName.propTypes)).to.have.length.above(0)
+//   })
 
-  it('renders an input element with the type `checkbox`', () => {
-    const checkbox = (
-      <Checkbox />
-    )
+//   it('renders an input element with the type `checkbox`', () => {
+//     const checkbox = (
+//       <Checkbox />
+//     )
 
-    expect(checkbox).to.equal(
-      <input class="checkbox" type="checkbox" />
-    )
-  })
+//     expect(checkbox).to.equal(
+//       <input class="checkbox" type="checkbox" />
+//     )
+//   })
 
-  it('applies the ID supplied in the `id` prop', () => {
-    const checkbox = (
-      <Checkbox id="foo" />
-    )
+//   it('applies the ID supplied in the `id` prop', () => {
+//     const checkbox = (
+//       <Checkbox id="foo" />
+//     )
 
-    expect(checkbox).to.equal(
-      <input id="foo" class="checkbox" type="checkbox" />
-    )
-  })
+//     expect(checkbox).to.equal(
+//       <input id="foo" class="checkbox" type="checkbox" />
+//     )
+//   })
 
-  it('is checked if the `value` prop is truthy', () => {
-    const checkbox = (
-      <Checkbox
-        value={true}
-      />
-    )
+//   it('is checked if the `value` prop is truthy', () => {
+//     const checkbox = (
+//       <Checkbox
+//         value={true}
+//       />
+//     )
 
-    expect(checkbox).to.equal(
-      <input class="checkbox" type="checkbox" checked />
-    )
-  })
+//     expect(checkbox).to.equal(
+//       <input class="checkbox" type="checkbox" checked />
+//     )
+//   })
 
-  it('is checked by default if the `value` prop is truthy', () => {
-    const callback = jest.fn()
+//   it('is checked by default if the `value` prop is truthy', () => {
+//     const callback = jest.fn()
 
-    mount(
-      <Checkbox
-        onChange={callback}
-      />
-    )
+//     mount(
+//       <Checkbox
+//         onChange={callback}
+//       />
+//     )
 
-    $('input').click()
+//     $('input').click()
 
-    expect(callback.mock.calls.length).to.equal(1)
-    expect(callback.mock.calls[0].length).to.equal(1)
-  })
-})
+//     expect(callback.mock.calls.length).to.equal(1)
+//     expect(callback.mock.calls[0].length).to.equal(1)
+//   })
+// })
