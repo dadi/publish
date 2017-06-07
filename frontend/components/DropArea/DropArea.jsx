93189d0e189a98a4cf9fb1be2e1e837af3d2197f
Eduardo Bouças <mail@eduardoboucas.com>
2017-06-07 17:37:26 +0100

    feat: add DropArea component

---
+++ frontend/components/DropArea/DropArea.jsx
@@ -0,0 +1,95 @@
+'use strict'
+
+import {h, Component} from 'preact'
+import proptypes from 'proptypes'
+
+import Style from 'lib/Style'
+import styles from './DropArea.css'
+
+export default class FileUpload extends Component {
+  static propTypes = {
+    /**
+     * File type acceptance.
+     */
+    accept: proptypes.string,
+
+    /**
+     * The contents of the drop area.
+     */
+    children: proptypes.node,
+
+    /**
+     * The text to be displayed when a file is being dragged.
+     */
+    draggingText: proptypes.string,
+
+    /**
+     * Whether to accept multiple files.
+     */
+    multiple: proptypes.bool,
+
+    /**
+     * Callback to be executed when files are dropped.
+     */
+    onDrop: proptypes.func
+  }
+
+  static defaultProps = {
+    draggingText: 'Drop files here',
+    multiple: false
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state.dragging = false
+  }
+
+  render() {
+    const {
+      accept,
+      children,
+      draggingText,
+      multiple
+    } = this.props
+    const {dragging} = this.state
+
+    const dropStyles = new Style(styles, 'droparea')
+      .addIf('droparea-active', dragging)
+
+    return (
+      <div
+        class={dropStyles.getClasses()}
+        data-dragtext={draggingText}
+        onDrop={this.handleDrop.bind(this)} 
+        onDragEnter={this.handleDrag.bind(this)}
+        onDragLeave={this.handleDrag.bind(this)}
+        onDragOver={this.handleDrag.bind(this)}
+      >
+        <div class={styles.contents}>
+          {children}
+        </div>
+      </div>
+    )
+  }
+
+  handleDrop(event) {
+    const {onDrop} = this.props
+
+    this.setState({
+      dragging: false
+    })
+
+    onDrop(event.dataTransfer.files)
+
+    event.preventDefault()
+  }
+
+  handleDrag(event) {
+    this.setState({
+      dragging: event.type !== 'dragleave'
+    })
+
+    event.preventDefault()
+  }
+}
