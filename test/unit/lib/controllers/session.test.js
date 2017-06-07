5b5b74b3254e53211d66deecf3361a8dd5e08e1c
mingard <info@arthurmingard.com>
2017-06-07 16:33:41 +0000

    feat: session testing

---
+++ test/unit/lib/controllers/session.test.js
@@ -0,0 +1,15 @@
+const globals = require(`${__dirname}/../../../../app/globals`) // Always required
+const Session = require(`${__dirname}/../../../../app/lib/controllers/session`).Session
+
+let session
+
+beforeEach(() => {
+  session = new Session()
+})
+
+describe('Session', () => {
+  it('should export function', () => {
+    expect(session).toBeInstanceOf(Object)
+  })
+})
+
