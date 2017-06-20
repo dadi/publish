35cb765e37127ce010c02fae5c841167b368e8a1
mingard <info@arthurmingard.com>
2017-06-20 10:13:15 +0000

    feat: check all valid, and invalid string values

--- test/unit/lib/helpers/regex.test.js
+++ test/unit/lib/helpers/regex.test.js
@@ -19,5 +19,41 @@ describe('Regex: URL', () => {
       expect(match[1]).toBe('http')
     })
   })
+
+  describe('URL test https', () => {
+    const https = 'https://example.com'
+    const match = regex.url.exec(https)
+    it (`Should not return null for '${https}'`, () => {
+      expect(match).not.toBeNull()
+    })
+    it (`Should have a first argument of '${https}'`, () => {
+      expect(match[0]).toBe('https://example.com')
+    })
+    it (`Should have a first argument of 'https'`, () => {
+      expect(match[1]).toBe('https')
+    })
+  })
+
+  describe('URL test ftp', () => {
+    const ftp = 'ftp://example.com'
+    const match = regex.url.exec(ftp)
+    it (`Should not return null for '${ftp}'`, () => {
+      expect(match).not.toBeNull()
+    })
+    it (`Should have a first argument of '${ftp}'`, () => {
+      expect(match[0]).toBe('ftp://example.com')
+    })
+    it (`Should have a first argument of 'ftp'`, () => {
+      expect(match[1]).toBe('ftp')
+    })
+  })
+
+  describe('URL test none passing string', () => {
+    const foo = 'foo://example.com'
+    const match = regex.url.exec(foo)
+    it (`Should not return null for '${foo}'`, () => {
+      expect(match).toBeNull()
+    })
+  })
 })
 
