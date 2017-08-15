d2dbdf0847a994ce696361dd60bbcf9ca80c196a
root <root@ip-172-31-44-0.eu-west-1.compute.internal>
2017-08-15 15:37:33 +0100

    test: remove unused test

--- test/unit/lib/controllers/api-bridge.test.js
+++ test/unit/lib/controllers/api-bridge.test.js
@@ -157,19 +157,19 @@ describe('ApiBridge', () => {
       apiBridge.post(req, res, () => {})
     })
 
-    it('should ...', (done) => {
-      req = httpMocks.createRequest({
-        headers,
-        method: 'POST',
-        body
-      })
-      req.isAuthenticated = () => true
-      res.on('end', () => {
+    // it('should ...', (done) => {
+      // req = httpMocks.createRequest({
+        // headers,
+        // method: 'POST',
+        // body
+      // })
+      // req.isAuthenticated = () => true
+      // res.on('end', () => {
         // console.log("DATA", res._getData())
-        done()
-      })
-      apiBridge.post(req, res, () => {})
-    })
+        // done()
+      // })
+      // apiBridge.post(req, res, () => {})
+    // })
   })
 
 })
