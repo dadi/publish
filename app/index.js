72073edbdaf6a2082c514232cebdcb5c14514670
mingard <info@arthurmingard.com>
2017-08-21 16:15:29 +0100

    test: index coverage

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@
 const globals = require('./globals') // eslint-disable-line
 
 const Server = require(paths.lib.server)
-const Watch = require(paths.lib.watch)
 let config = require(paths.config) // eslint-disable-line
 
 /*
@@ -18,22 +17,11 @@ const App = function () {}
 
 /**
  * Start Publish App
- * @return {Server}        Server Instace
+ * @return {Server}        Server Instance
  */
 App.prototype.start = function (options = {isStandalone: false}) {
-  if (!options.isStandalone) {
-    new Watch().start()
-  }
   this.server = new Server()
   return this.server.start()
-}
-
-/**
- * Stop Publish App
- * @return {Promise} app.close promise
- */
-App.prototype.stop = function () {
-  return this.server.stop()
 }
 
 module.exports = new App()
---
+++ test/unit/index.test.js
@@ -0,0 +1,24 @@
+const globals = require(`${__dirname}/../../app/globals`) // Always required
+const app = require(`${__dirname}/../../app`)
+const Server = require(`${__dirname}/../../app/lib/server`).Server
+
+describe('App', () => {
+  it('should export object', () => {
+    expect(app).toBeInstanceOf(Object)
+  })
+
+  describe('start()', () => {
+    it('should set property this.server.', () => {
+      app.start()
+      expect(app.server)
+        .toBeInstanceOf(Server)
+    })
+
+    it('should return a promise.', () => {
+      app.start()
+      expect(app.start())
+        .toBeInstanceOf(Promise)
+    })
+  })
+})
+
