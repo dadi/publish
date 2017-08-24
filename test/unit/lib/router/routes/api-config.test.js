122f16fe836d921680128e29630a4d96d4e317f4
mingard <info@arthurmingard.com>
2017-08-24 13:21:46 +0100

    test: api-config route coverage

--- test/unit/lib/router/routes/api-config.test.js
+++ test/unit/lib/router/routes/api-config.test.js
@@ -1,5 +1,5 @@
 const globals = require(`${__dirname}/../../../../../app/globals`) // Always required
-const APIBridgeRoutes = require(`${__dirname}/../../../../../app/lib/router/routes/api-bridge`)
+const AppConfigRoutes = require(`${__dirname}/../../../../../app/lib/router/routes/app-config`)
 const nock = require('nock')
 const restify = require('restify')
 
@@ -16,17 +16,11 @@ beforeEach(() => {
 
 describe('Router', () => {
   it('should export object', () => {
-    expect(new APIBridgeRoutes(server)).toBeInstanceOf(Object)
+    expect(new AppConfigRoutes(server)).toBeInstanceOf(Object)
   })
 
   it('should exit if server is undefined', () => {
-    const sessionRouteInstance = new APIBridgeRoutes()
+    const sessionRouteInstance = new AppConfigRoutes()
     expect(sessionRouteInstance).toEqual({})
-  })
-
-  it('should exit if server is undefined', () => {
-    new APIBridgeRoutes(server)
-    expect(restifySpy)
-      .toHaveBeenCalled()
   })
 })
