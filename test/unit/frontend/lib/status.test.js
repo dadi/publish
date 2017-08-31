70067f1b4937b829ca75ae8a6b1cca08e7cb94da
mingard <info@arthurmingard.com>
2017-08-31 17:33:09 +0100

    test: status coverage for isOnline

---
+++ test/unit/frontend/lib/status.test.js
@@ -0,0 +1,39 @@
+const globals = require(`${__dirname}/../../../../app/globals`) // Always required
+const Status = require(`${__dirname}/../../../../frontend/lib/status`)
+
+Object.defineProperty(window.navigator, "onLine", (function(_value){
+  return {
+    get: function _get() {
+      return _value;
+    },
+    set: function _set(v) {
+        _value = v;
+    }
+  };
+})(window.navigator.onLine));
+
+beforeEach(() => {
+  window.navigator.onLine = true
+})
+
+describe('Status', () => {
+  it('should be an Object', () => {
+    expect(Status).toMatchObject(expect.any(Object))
+  })
+
+  describe('isOnline()', () => {
+    it('should return true if window.navigator.onLine is true', () => {
+      expect(Status.isOnline()).toBeTruthy()
+    })
+
+    it('should return true if window.navigator.onLine is false but is defined', () => {
+      window.navigator.onLine = false
+      expect(Status.isOnline()).toBeTruthy()
+    })
+
+    it('should return false if window.navigator.onLine is undefined', () => {
+      window.navigator.onLine = undefined
+      expect(Status.isOnline()).toBeFalsy()
+    })
+  })
+})
