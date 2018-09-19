f2b512856bd3a56507f76d0c9224d5875f3bd636
Anton Naumovets <an@dadi.co>
2018-09-19 19:38:46 +0300

    patch: disable component tree plugin in storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,9 @@
 'use strict'
+const ComponentTreePlugin = require('component-tree-webpack-plugin')
 
 let config = require('../webpack.config')
+
+config.plugins = config.plugins.filter(plugin => !(plugin instanceof ComponentTreePlugin))
 
 config.resolve.alias['react'] =  'preact-compat'
 config.resolve.alias['react-dom'] =  'preact-compat'
