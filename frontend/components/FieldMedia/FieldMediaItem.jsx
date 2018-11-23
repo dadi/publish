8d58b04656709441f0d057ced65b1bd5f41c954a
Eduardo Bouças <mail@eduardoboucas.com>
2018-11-23 14:32:34 +0000

    refactor: remove Jim’s code :knife:

--- frontend/components/FieldMedia/FieldMediaItem.jsx
+++ frontend/components/FieldMedia/FieldMediaItem.jsx
@@ -27,7 +27,7 @@ export default class FieldMediaItem extends Component {
     const {config, isList, value} = this.props
     const cdn = config ? config.cdn : null
 
-    if (!value || !value.fileName) {
+    if (!value) {
       return null
     }
 
