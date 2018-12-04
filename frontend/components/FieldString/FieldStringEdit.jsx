ab19dd52bf09b7f6612f629541e5cfaae6ab02e5
Eduardo Bouças <mail@eduardoboucas.com>
2018-12-04 15:17:35 +0000

    Update frontend/components/FieldString/FieldStringEdit.jsx
    Co-Authored-By: jimlambie <jameslambie@gmail.com>

--- frontend/components/FieldString/FieldStringEdit.jsx
+++ frontend/components/FieldString/FieldStringEdit.jsx
@@ -373,7 +373,7 @@ export default class FieldStringEdit extends Component {
             size="small"
             href={linkFormatted || value} 
             className={styles['link-preview']}
-            openInNewWindow="true"
+            openInNewWindow={true}
           >Open in new window</Button>
         )}
       </Label>
