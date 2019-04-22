4f81cbcc65a0dda9aacdbb38ba1e01f53cc96f9f
Eduardo Bouças <mail@eduardoboucas.com>
2019-04-22 15:31:37 +0100

    fix: adhere to collection menu sorting

--- frontend/containers/CollectionNav/CollectionNav.jsx
+++ frontend/containers/CollectionNav/CollectionNav.jsx
@@ -1,17 +1,11 @@
-import {Component, h} from 'preact'
-import {Router} from '@dadi/preact-router'
-import proptypes from 'proptypes'
-import {connect} from 'preact-redux'
-import {bindActionCreators} from 'redux'
-
 import * as apiActions from 'actions/apiActions'
 import * as appActions from 'actions/appActions'
-
-import Nav from 'components/Nav/Nav'
-
-import {buildUrl} from 'lib/router'
+import {bindActionCreators} from 'redux'
+import {Component, h} from 'preact'
 import {connectHelper} from 'lib/util'
 import {Format} from 'lib/util/string'
+import Nav from 'components/Nav/Nav'
+import proptypes from 'proptypes'
 
 class CollectionNav extends Component {
   static propTypes = {
@@ -26,165 +20,103 @@ class CollectionNav extends Component {
     state: proptypes.object
   }
 
-  buildCollectionMap(apis) {
-    let groups = {}
-    let ungrouped = {}
+  getNavigationItems(api) {
+    if (!api || !Array.isArray(api.collections)) {
+      return []
+    }
 
-    apis.forEach(api => {
-      if (!api.collections) return
+    // There are some collections that we don't want to display on the menu,
+    // like legacy media collections.
+    const collections = api.collections.filter(collection => {
+      const isMediaCollection = collection.settings &&
+        collection.settings.type === 'media'
 
-      let apiCollections = {}
-      let displayNames = {}
-
-      // There are some collections that we don't want to display on the menu,
-      // like auth or media collections.
-      const filteredCollections = api.collections.filter(collection => {
-        const isMediaCollection = collection.settings &&
-          collection.settings.type === 'media'
-
-        return !collection._isAuthCollection && !isMediaCollection
-      })
-
-      // We start by adding all the collections that are referenced in the menu
-      // object.
-      let menu = (api.menu || [])
-      let slugs = filteredCollections.map(collection => collection.slug)
-
-      menu.forEach(menuEntry => {
-        //1st level menu entry
-        if (!menuEntry.collections) {
-          if (slugs.includes(menuEntry)) {
-            apiCollections[menuEntry] = null
-          }
-          return
-        }
-
-        //nested menu entry
-        menuEntry.collections.forEach(menuCollection => {
-          if (slugs.includes(menuCollection)) {
-            apiCollections[menuCollection] = menuEntry.title
-          }
-        })
-      })
-
-      // Then we loop through all collections and add any that is missing from the
-      // menu.
-      filteredCollections.forEach(collection => {
-        apiCollections[collection.slug] = apiCollections[collection.slug] || null
-
-        const displayName = (collection.settings && collection.settings.displayName) || collection.name
-
-        displayNames[collection.slug] = displayName
-      })
-
-      // We then merge the collections map for this API with the global map.
-      Object.keys(apiCollections).forEach(collection => {
-        const mapValue = apiCollections[collection]
-
-        // Is it part of a group?
-        if (mapValue) {
-          // Create group and collection levels (if they don't exist).
-          groups[mapValue] = groups[mapValue] || {}
-          groups[mapValue][collection] = groups[mapValue][collection] || []
-
-          // Increment the number of matches at the given group+collection.
-          groups[mapValue][collection].push(displayNames[collection])
-        } else {
-          ungrouped[collection] = ungrouped[collection] || []
-          ungrouped[collection].push(displayNames[collection])
-        }
-      })
+      return !isMediaCollection
     })
 
-    return {
-      groups,
-      ungrouped
-    }
-  }
+    let navItems = []
+    let processedSlugs = {}
+    
+    const menu = Array.isArray(api.menu) ? api.menu : []
 
-  buildGroups(collectionMap) {
-    const grouped = Object.keys(collectionMap.groups).map(groupTitle => {
-      const group = collectionMap.groups[groupTitle]
-      const groupSlug = Format.slugify(groupTitle)
+    menu.forEach(item => {
+      // Is this entry a top-level collection?
+      if (typeof item === 'string') {
+        const match = collections.find(({slug}) => slug === item)
 
-      let subItems = []
-
-      Object.keys(group).forEach(collection => {
-        group[collection].forEach((displayName, i) => {
-          const collectionSlug = collection + ((i > 0) ? `-${i + 1}` : '')
-
-          subItems.push({
-            id: `${groupSlug}/${collectionSlug}`,
-            label: displayName,
-            href: buildUrl(groupSlug, collectionSlug)
+        if (match) {
+          navItems.push({
+            id: match.slug,
+            label: match.displayName || match.slug,
+            href: match._publishLink
           })
-        })
-      })
 
-      return {
-        id: groupSlug,
-        label: groupTitle,
-        subItems,
-        href: subItems[0].href
+          processedSlugs[match.slug] = true
+        }
+
+        return
+      }
+
+      // Is this entry a collection group?
+      if (Array.isArray(item.collections) && item.title) {
+        const groupSlug = Format.slugify(item.title)
+        const groupCollections = item.collections.map(item => {
+          const match = collections.find(({slug}) => slug === item)
+
+          if (match) {
+            const {_publishLink, settings = {}, slug} = match
+
+            processedSlugs[slug] = true
+
+            return {
+              id: slug,
+              label: settings.displayName || slug,
+              href: _publishLink
+            }
+          }
+        }).filter(Boolean)
+
+        if (groupCollections.length > 0) {
+          navItems.push({
+            label: item.title,
+            subItems: groupCollections
+          })
+        }
       }
     })
 
-    let ungrouped = []
+    // Adding to the nav any collections that haven't been included in the menu
+    // config object.
+    collections.forEach(collection => {
+      if (processedSlugs[collection.slug]) return
 
-    Object.keys(collectionMap.ungrouped).forEach(collection => {
-      const numberOfInstances = collectionMap.ungrouped[collection]
+      const {_publishLink, settings = {}, slug} = collection
 
-      collectionMap.ungrouped[collection].forEach((displayName, i) => {
-        const collectionSlug = collection + ((i > 0) ? `-${i + 1}` : '')
-
-        ungrouped.push({
-          id: collectionSlug,
-          label: displayName,
-          href: buildUrl(null, collectionSlug)
-        })
+      navItems.push({
+        id: slug,
+        label: settings.displayName || slug,
+        href: _publishLink
       })
     })
 
-    // Add static menu item for media.
-    ungrouped.push({
+    return navItems
+  }
+
+  render() {
+    const {currentCollection, state} = this.props
+    const apis = state.api.apis
+    const items = this.getNavigationItems(apis[0])
+
+    items.push({
       id: '_media',
       label: 'Media Library',
       href: '/media'
     })
 
-    return grouped.concat(ungrouped)
-  }
-
-  createStructure() {
-    const {state, actions} = this.props
-    const apis = state.api.apis
-
-    if (apis && apis.length) {
-      const collectionMap = this.buildCollectionMap(apis)
-
-      this.groups = this.buildGroups(collectionMap)
-    }
-  }
-
-  componentWillMount() {
-    this.createStructure()
-  }
-
-  componentWillUpdate() {
-    this.createStructure()
-  }
-
-  render() {
-    const {currentCollection, state, actions} = this.props
-
-    if (!this.groups) {
-      return null
-    }
-
     return (
       <Nav
         currentCollection={currentCollection}
-        items={this.groups}
+        items={items}
         mobile={state.app.breakpoint === null}
       />
     )
