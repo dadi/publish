121ec2899c054ef9c5be3f7dab397d5f431f7322
Moses <moses.skoda@gmail.com>
2019-09-08 19:19:30 +0200

    merge Nav into CollectionNav

--- app/containers/CollectionNav/CollectionNav.jsx
+++ app/containers/CollectionNav/CollectionNav.jsx
@@ -1,7 +1,9 @@
 import buildGroupedMenuItems from './buildGroupedMenuItems'
 import {connectRedux} from 'lib/redux'
 import {connectRouter} from 'lib/router'
-import Nav from 'components/Nav/Nav'
+import Dropdown from 'components/Dropdown/Dropdown'
+import DropdownItem from 'components/Dropdown/DropdownItem'
+import NavItem from 'components/NavItem/NavItem'
 import React from 'react'
 
 class CollectionNav extends React.Component {
@@ -18,11 +20,60 @@ class CollectionNav extends React.Component {
       }
     } = this.props
 
+    const items = buildGroupedMenuItems(api, collection)
+    const mobile = breakpoint === null
+
     return (
-      <Nav
-        items={buildGroupedMenuItems(api, collection)}
-        mobile={breakpoint === null}
-      />
+      <nav>
+        <ul>
+          {items.map(item => {
+            let subItems = null
+
+            if (item.subItems) {
+              const children = item.subItems.map(subItem => {
+                if (mobile) {
+                  return (
+                    <NavItem
+                      href={subItem.href}
+                      key={subItem.href}
+                      mobile={true}
+                      text={subItem.label}
+                    />
+                  )
+                }
+
+                return (
+                  <DropdownItem
+                    active={subItem.isSelected}
+                    href={subItem.href}
+                    key={subItem.href}
+                  >
+                    {subItem.label}
+                  </DropdownItem>
+                )
+              })
+
+              subItems = mobile ? (
+                <ul>{children}</ul>
+              ) : (
+                <Dropdown>{children}</Dropdown>
+              )
+            }
+
+            return (
+              <NavItem
+                active={item.isSelected}
+                href={item.href}
+                key={item.label + item.href}
+                text={item.label}
+                mobile={mobile}
+              >
+                {subItems}
+              </NavItem>
+            )
+          })}
+        </ul>
+      </nav>
     )
   }
 }
