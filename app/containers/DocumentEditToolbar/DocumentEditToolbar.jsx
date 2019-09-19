7d28da7d31076b42fdecc5427eea331d2678521f
Moses <moses.skoda@gmail.com>
2019-09-19 18:53:46 +0200

    reskin edit view toolbar

--- app/containers/DocumentEditToolbar/DocumentEditToolbar.jsx
+++ app/containers/DocumentEditToolbar/DocumentEditToolbar.jsx
@@ -2,18 +2,18 @@ import * as appActions from 'actions/appActions'
 import * as Constants from 'lib/constants'
 import * as documentActions from 'actions/documentActions'
 import * as userActions from 'actions/userActions'
-import {Button, ButtonWithOptions} from '@dadi/edit-ui'
+import {Button, ButtonWithOptions, Select} from '@dadi/edit-ui'
 import {connectRedux} from 'lib/redux'
 import {connectRouter} from 'lib/router'
 import DateTime from 'components/DateTime/DateTime'
-import DropdownNative from 'components/DropdownNative/DropdownNative'
+import {ExpandMore} from '@material-ui/icons'
 import HotKeys from 'lib/hot-keys'
 import Modal from 'components/Modal/Modal'
 import Prompt from 'components/Prompt/Prompt'
 import proptypes from 'prop-types'
 import React from 'react'
+import Style from 'lib/Style'
 import styles from './DocumentEditToolbar.css'
-import Toolbar from 'components/Toolbar/Toolbar'
 
 /**
  * A toolbar used in a document edit view.
@@ -66,6 +66,7 @@ class DocumentEditToolbar extends React.Component {
     super(props)
 
     this.handleDelete = this.handleDelete.bind(this)
+    this.handleLanguageChange = this.handleLanguageChange.bind(this)
     this.hotkeys = new HotKeys({
       'mod+s': this.handleSave.bind(this, null)
     })
@@ -73,8 +74,11 @@ class DocumentEditToolbar extends React.Component {
 
     this.showDeletePrompt = () => this.setState({isShowingDeletePrompt: true})
     this.hideDeletePrompt = () => this.setState({isShowingDeletePrompt: false})
+    this.toggleExpand = () =>
+      this.setState(({isExpanded}) => ({isExpanded: !isExpanded}))
 
     this.state = {
+      isExpanded: false,
       isShowingDeletePrompt: false
     }
   }
@@ -93,7 +97,7 @@ class DocumentEditToolbar extends React.Component {
         this,
         Constants.SAVE_ACTION_SAVE_AND_CONTINUE
       ),
-      label: 'Save & continue'
+      label: 'Save'
     }
     const secondary = [
       {
@@ -142,12 +146,10 @@ class DocumentEditToolbar extends React.Component {
     }
   }
 
-  handleLanguageChange(newLanguage) {
+  handleLanguageChange(event) {
     const {onBuildBaseUrl, route} = this.props
     const redirectUrl = onBuildBaseUrl({
-      search: {
-        lang: newLanguage
-      }
+      search: {lang: event.target.value}
     })
 
     route.history.push(redirectUrl)
@@ -181,123 +183,132 @@ class DocumentEditToolbar extends React.Component {
       Object.keys(validationErrors).filter(field => validationErrors[field])
         .length
     const saveOptions = this.getSaveOptions(documentId)
-    const languages =
-      api.languages.length <= 1
-        ? null
-        : api.languages.reduce((languagesObject, language) => {
-            languagesObject[language.code] = language.name
 
-            return languagesObject
-          }, {})
+    const showLanguageSelector = multiLanguage && api.languages.length > 1
+    const languages = api.languages.map(({code, name}) => ({
+      value: code,
+      label: name
+    }))
 
     let {lang: currentLanguage} = route.search
 
     // No language is selected, so we'll set the value of the dropdown to the
     // value of the default language.
-    if (languages && !currentLanguage) {
-      const defaultLanguage = api.languages.find(language => {
-        return Boolean(language.default)
-      })
+    if (showLanguageSelector && !currentLanguage) {
+      const defaultLanguage = api.languages.find(language => language.default)
 
       currentLanguage = defaultLanguage && defaultLanguage.code
     }
 
+    const {isExpanded} = this.state
+    const containerStyle = new Style(styles, 'container').addIf(
+      'expanded',
+      isExpanded
+    )
+
     return (
-      <Toolbar>
-        {multiLanguage && languages && (
-          <DropdownNative
-            className={styles['language-dropdown']}
-            onChange={this.handleLanguageChange.bind(this)}
-            options={languages}
-            value={currentLanguage}
-          />
-        )}
-
-        <div className={styles.metadata}>
-          {remote && remote._createdAt && (
-            <p>
-              <span>Created </span>
-
-              {remote._createdBy && (
-                <span>
-                  by <strong>{remote._createdBy}</strong>{' '}
-                </span>
-              )}
-
-              <DateTime date={remote._createdAt} relative={true} />
-            </p>
-          )}
-
-          {remote && remote._lastModifiedAt && (
-            <p>
-              <span>Last updated </span>
-
-              {remote._lastModifiedBy && (
-                <span>
-                  by <strong>{remote._lastModifiedBy}</strong>{' '}
-                </span>
-              )}
-
-              <DateTime date={remote._lastModifiedAt} relative={true} />
-            </p>
-          )}
+      <footer className={containerStyle.getClasses()}>
+        <div className={styles.expand}>
+          <button onClick={this.toggleExpand}>
+            <ExpandMore fontSize="large" />
+          </button>
         </div>
-
-        <div className={styles.buttons}>
-          {remote && !isSingleDocument && (
-            <div className={styles.button}>
-              <Button
-                accent="negative"
-                disabled={Boolean(hasConnectionIssues)}
-                filled
-                onClick={this.showDeletePrompt}
-              >
-                Delete
-              </Button>
-            </div>
+        <div className={styles.content}>
+          {showLanguageSelector && (
+            <Select
+              className={styles['language-selector']}
+              onChange={this.handleLanguageChange}
+              options={languages}
+              value={currentLanguage}
+            />
           )}
 
-          {this.state.isShowingDeletePrompt && (
-            <Modal onRequestClose={this.hideDeletePrompt}>
-              <Prompt
-                accent="negative"
-                action={'Yes, delete it'}
-                onCancel={this.hideDeletePrompt}
-                onConfirm={this.handleDelete}
-              >
-                Are you sure you want to delete this document?
-              </Prompt>
-            </Modal>
-          )}
+          <div className={styles.metadata}>
+            {remote && remote._createdAt && (
+              <div>
+                <span>Created </span>
 
-          <div className={styles.button}>
-            {isSingleDocument ? (
-              <Button
-                accent="positive"
-                disabled={Boolean(
-                  hasConnectionIssues || hasValidationErrors || isSaving
+                {remote._createdBy && (
+                  <span>
+                    by <strong>{remote._createdBy}</strong>{' '}
+                  </span>
                 )}
-                isLoading={isSaving}
-                onClick={saveOptions.primary.action}
-              >
-                Save document
-              </Button>
-            ) : (
-              <ButtonWithOptions
-                accent="positive"
-                disabled={Boolean(
-                  hasConnectionIssues || hasValidationErrors || isSaving
+
+                <DateTime date={remote._createdAt} relative={true} />
+              </div>
+            )}
+
+            {remote && remote._lastModifiedAt && (
+              <div>
+                <span>Last updated </span>
+
+                {remote._lastModifiedBy && (
+                  <span>
+                    by <strong>{remote._lastModifiedBy}</strong>{' '}
+                  </span>
                 )}
-                isLoading={isSaving}
-                onClick={saveOptions.primary.action}
-                options={saveOptions.secondary}
-              >
-                {saveOptions.primary.label}
-              </ButtonWithOptions>
+
+                <DateTime date={remote._lastModifiedAt} relative={true} />
+              </div>
             )}
           </div>
+
+          <div className={styles.buttons}>
+            {remote && !isSingleDocument && (
+              <div className={styles.button}>
+                <Button
+                  accent="negative"
+                  disabled={Boolean(hasConnectionIssues)}
+                  filled
+                  onClick={this.showDeletePrompt}
+                >
+                  Delete
+                </Button>
+              </div>
+            )}
+
+            {this.state.isShowingDeletePrompt && (
+              <Modal onRequestClose={this.hideDeletePrompt}>
+                <Prompt
+                  accent="negative"
+                  action={'Yes, delete it'}
+                  onCancel={this.hideDeletePrompt}
+                  onConfirm={this.handleDelete}
+                >
+                  Are you sure you want to delete this document?
+                </Prompt>
+              </Modal>
+            )}
+
+            <div className={styles.button}>
+              {isSingleDocument ? (
+                <Button
+                  accent="positive"
+                  disabled={Boolean(
+                    hasConnectionIssues || hasValidationErrors || isSaving
+                  )}
+                  isLoading={isSaving}
+                  onClick={saveOptions.primary.action}
+                >
+                  Save document
+                </Button>
+              ) : (
+                <ButtonWithOptions
+                  accent="positive"
+                  disabled={Boolean(
+                    hasConnectionIssues || hasValidationErrors || isSaving
+                  )}
+                  isLoading={isSaving}
+                  onClick={saveOptions.primary.action}
+                  options={saveOptions.secondary}
+                >
+                  {saveOptions.primary.label}
+                </ButtonWithOptions>
+              )}
+            </div>
+          </div>
         </div>
-      </Toolbar>
+      </footer>
     )
   }
 
